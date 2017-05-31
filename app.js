1ef83fdc3dc904a217d500611b2f4a2e0eeafe25
PriyankaGanesh-L <mail.priyankalakshmanan@gmail.com>
2017-05-31 22:22:55 +0530

    Assignment 5.1
    Run using chrome console.

---
+++ app.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+var name = 'Priyanka';
+var age = 25;
+var dob = new Date("2000-03-25").toDateString();
+var pob = 'Chennai'
+console.log("Student Details")
+console.log("The name is " + name);
+console.log("The age is " + age);
+console.log("The dob is " + dob);
+console.log("The place of birth is " + pob);
+
+
+
